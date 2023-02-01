94a70d32dedc21447095ed47037f7ec12814159a
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-01 16:57:08 -0300

    Adicionado novos exercícios

---
+++ Exercicios/Exercicio06/src/main/java/com/mycompany/exercicio06/Exercicio06.java
@@ -0,0 +1,16 @@
+package com.mycompany.exercicio06;
+
+public class Exercicio06 {
+
+    public static void main(String[] args) {
+        professor p1 = new professor();
+        p1.nome = "Lucas de Sena";
+        p1.curso = "Programação Orientada a Objetos";
+        p1.imprimiProfessor();
+        
+        professor p2 = new professor();
+        p2.nome = "Jhonny";
+        p2.curso = "Lógica de programação";
+        p2.imprimiProfessor();
+    }
+}
---
+++ Exercicios/Exercicio06/src/main/java/com/mycompany/exercicio06/professor.java
@@ -0,0 +1,10 @@
+package com.mycompany.exercicio06;
+
+public class professor {
+    String nome;
+    String curso;
+    
+    public void imprimiProfessor(){
+        System.out.println("Olá, eu sou o professor " + nome + " e você está no curso de " + curso);
+    }
+}
