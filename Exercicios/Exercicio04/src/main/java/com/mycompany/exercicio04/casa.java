2e798f5db1ad71d3e3048f4c441290c9444ea6c4
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-01 16:55:34 -0300

    Adicionado novos exercícios

---
+++ Exercicios/Exercicio04/src/main/java/com/mycompany/exercicio04/casa.java
@@ -0,0 +1,10 @@
+package com.mycompany.exercicio04;
+
+public class casa {
+    int metrosQuadrados;
+    String cor;
+    
+    public void imprimirDetalhesCasa(){
+        System.out.println("Sua casa possui " + metrosQuadrados + "m² e a cor dela é: " + cor);
+    }
+}
