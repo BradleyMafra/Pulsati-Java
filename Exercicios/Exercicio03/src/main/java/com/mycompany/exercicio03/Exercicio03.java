7083e50e5a15c66b8940723fb31cd24974f7d13d
Bradley Mafra <53132092+BradleyMafra@users.noreply.github.com>
2023-02-01 16:55:10 -0300

    Adicionado novos exercícios

---
+++ Exercicios/Exercicio03/src/main/java/com/mycompany/exercicio03/Exercicio03.java
@@ -0,0 +1,16 @@
+package com.mycompany.exercicio03;
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+        aluno aluno1 = new aluno();
+        aluno1.nome = "Bradley";
+        aluno1.nota = 10;
+        aluno1.imprimirAluno();
+        
+        aluno aluno2 = new aluno();
+        aluno2.nome = "Natan";
+        aluno2.nota = 5.5;
+        aluno2.imprimirAluno();
+        }
+}
